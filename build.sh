9600ad466dac5d579279d109236b2771f8ce9098
ATTY Lionel <lionel.atty@ign.fr>
2016-11-29 17:00:22 +0100

    Typo.

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-docker build --build-arg http_proxy=http://proxy.ign.fr:3128 --build-arg HTTP_PROXY=http://proxy.ign.fr:3128 --build-arg https_proxy=http://proxy.ign.fr:3128 --tag arduino_with_xvfb .
+docker build \
+    --build-arg http_proxy=http://proxy.ign.fr:3128 \
+    --build-arg HTTP_PROXY=http://proxy.ign.fr:3128 \
+    --build-arg https_proxy=http://proxy.ign.fr:3128 \
+    --tag arduino/xfvb:1.6.13 \
+    .
