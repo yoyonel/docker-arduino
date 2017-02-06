b6c6bea7cd6ae75b597ad27380cce8b11d637530
Lionel ATTY <lionel.atty@gmail.com>
2017-02-06 16:03:13 +0100

    Add scripts directory and entry-point script to set env. var.

---
+++ entry-point.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PATH=$PATH:$(realpath scripts/)
+ls scripts/ -1
+
---
+++ scripts/launch_run_bash.sh
@@ -0,0 +1,1 @@
+docker run -it --network=arduino_default --env ROS_HOSTNAME=arduino_client --env ROS_MASTER_URI=http://master:11311 arduino_arduino /bin/bash
