95b5d368a889d500e13d00cf3ba4a3ec0fe6ed75
Lionel ATTY <lionel.atty@gmail.com>
2017-02-08 19:07:31 +0100

    Formatage de scripts/inspect.sh

--- scripts/inspect.sh
+++ scripts/inspect.sh
@@ -1,1 +1,7 @@
-docker run -it --network=arduino_default --env ROS_HOSTNAME=arduino_client --env ROS_MASTER_URI=http://master:11311 arduino_arduino /bin/bash
+#!/bin/sh
+docker run -it \
+    --network=arduino_default \
+    --env ROS_HOSTNAME=arduino_client \
+    --env ROS_MASTER_URI=http://master:11311 \
+    arduino_arduino \
+    /bin/bash
