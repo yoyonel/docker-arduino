3a09ed1623968298ef32877fd74d1836b513c1fd
yoyonel <yoyonel@hotmail.com>
2017-02-09 22:55:34 +0100

    Rajout des sources 'docker-arduino'. A nettoyer par la suite

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -3,5 +3,7 @@ docker run -it \
     --network=dockerarduino_default \
     --env ROS_HOSTNAME=arduino_client \
     --env ROS_MASTER_URI=http://master:11311 \
+    --device=/dev/ttyACM0:/dev/ttyACM0 \
+    -v /home/atty/Prog/__IGN__/__DOCKER__/docker-arduino/docker-arduino/project/LI3DS_ARDUINO:/root/LI3DS_ARDUINO \
     dockerarduino_arduino \
     /bin/bash
